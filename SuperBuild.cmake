23a3bdb0e124d8c27d253fc3aa261b04c5810b94
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-04-09 10:51:34 -0400

    ENH: Backport SurfaceToolbox module updates from SlicerSALT fork
    This commit updates SlicerSurfaceToolbox to backport the following modules
    from the SlicerSALT fork:
    - AverageMesh
    - MeshDataImprinter
    - SurfaceDistance
    - SurfaceFeatureExtractor
    
    It also backports improvements associated with the MeshAlignment module.
    
    These changes correspond to those proposed in the following pull requests
    which are still under review and not yet merged upstream:
    - Slicer/SlicerSurfaceToolbox#71
    - Slicer/SlicerSurfaceToolbox#72
    - Slicer/SlicerSurfaceToolbox#73
    - Slicer/SlicerSurfaceToolbox#74
    - Slicer/SlicerSurfaceToolbox#75
    
    List of changes:
    
    ```
    $ git shortlog 26715cf..389b220 --no-merges
    Ye Han (5):
          [Backport PR-71] ENH:Add MeshAlignment module
          [Backport PR-72] ENH: Add MeshDataImprinter module
          [Backport PR-73] ENH: Add SurfaceFeatureExtractor module
          [Backport PR-74] ENH: Add SurfaceDistance Module
          [Backport PR-75] ENH: Add AverageMesh module
    ```

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -338,7 +338,7 @@ list_conditional_append(Slicer_BUILD_LandmarkRegistration Slicer_REMOTE_DEPENDEN
 
 Slicer_Remote_Add(SurfaceToolbox
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/slicersalt/SlicerSurfaceToolbox"
-  GIT_TAG 26715cf15e90bfd23fcbe35b09581bddb6267360 # slicersalt-2025-01-05-e8b8f70
+  GIT_TAG 389b2204d3da496bf33bb350a5a1f0070707e584 # slicersalt-2025-01-05-e8b8f70
   OPTION_NAME Slicer_BUILD_SurfaceToolbox
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
